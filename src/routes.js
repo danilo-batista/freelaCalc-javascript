4562325fdd06038165079aa11697a49b5321e615
danilo-batista <developer@danilobatista.com>
2021-03-28 12:00:45 -0300

    Discover | FreelaCalc: First class(part2), applying the EJS template engine .

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const routes = express.Router();
 
-const basePath = __dirname + "/views";
+const views = __dirname + "/views/";
 
-routes.get('/', (request, response) => { return response.sendFile(basePath + "/index.html"); });
-routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"));
-routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"));
-routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"));
+const profile = {
+    name: "Danilo Batista",
+    avatar: "https://avatars.githubusercontent.com/u/60406813?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 8,
+    "vacation-per-year": 4
+}
+
+routes.get('/', (request, response) => { return response.render(views + "index"); });
+routes.get('/job', (request, response) => response.render(views + "job"));
+routes.get('/job/edit', (request, response) => response.render(views + "job-edit"));
+routes.get('/profile', (request, response) => response.render(views + "profile", { profile } ));
 
 module.exports = routes;
